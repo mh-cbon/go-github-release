336e402664ba42e2d2dddae432dfe61c038088a5
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-22 12:31:18 +0200

    update

--- .version.sh
+++ .version.sh
@@ -1,5 +1,5 @@
 PREBUMP=
   666 rm -fr src/.git
   666 git init src
-  666 emd gen README.e.md > README.md
+  666 emd gen README.e.md -out README.md
   666 commit -q -m "README: !newversion!" -f README.md
